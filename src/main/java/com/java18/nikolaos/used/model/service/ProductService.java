1ae238037acdcb51478c8154cb208d8453c5589c
Wendy <romiling923@gmail.com>
2022-09-29 10:15:15 +0800

    add productLaunch.jsp & MemberProducts.jsp, modified product part

--- src/main/java/com/java18/nikolaos/used/model/service/ProductService.java
+++ src/main/java/com/java18/nikolaos/used/model/service/ProductService.java
@@ -3,16 +3,16 @@ package com.java18.nikolaos.used.model.service;
 import java.util.HashMap;
 import java.util.List;
 
-import com.java18.nikolaos.used.model.Members;
-import com.java18.nikolaos.used.model.UsedCategory;
 import com.java18.nikolaos.used.model.UsedProduct;
 
 public interface ProductService {
 
-	UsedProduct createProduct(String name, Integer price, String content, Members memberId, UsedCategory categoryId,
+	UsedProduct createProduct(String name, Integer price, String content, Integer memberId, Integer categoryId,
 			String cover, String status);
 
 	UsedProduct getProduct(Integer id);
+	
+	List<UsedProduct> getProductListByMemberId(Integer memberId);
 
 	List<UsedProduct> getProductsByParentId(Integer categoryId);
 
--- src/main/webapp/WEB-INF/views/include/TopBar.jsp
+++ src/main/webapp/WEB-INF/views/include/TopBar.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8"%>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Nikolaos</a>
+  <a class="navbar-brand" href="/nikolaos/">Nikolaos</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
